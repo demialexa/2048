fb896af29772822730081ac668b96d9936f4d2e7
Vladimir Zhukov <zhvv117@gmail.com>
2018-10-10 00:54:05 +0300

    Initial commit

---
+++ src/bin/main.cpp
@@ -0,0 +1,48 @@
+#include <display/display.h>
+
+#include <cmath>
+
+int main() {
+    TDisplay display;
+
+    double y = 0;
+    double oldTime = display.GetTime();
+    while (!display.Closed()) {
+        display.ProcessEvents();
+
+        // TODO: Insert your code here
+
+        // Example: draw tiles
+        display.DrawTile(0, 0, ETileType::TILE_1);
+        display.DrawTile(0, 1, ETileType::TILE_2);
+        display.DrawTile(0, 2, ETileType::TILE_4);
+        display.DrawTile(0, 3, ETileType::TILE_8);
+        display.DrawTile(1, 0, ETileType::TILE_16);
+        display.DrawTile(1, 1, ETileType::TILE_32);
+        display.DrawTile(1, 2, ETileType::TILE_64);
+        display.DrawTile(1, 3, ETileType::TILE_128);
+        display.DrawTile(2, 0, ETileType::TILE_256);
+        display.DrawTile(2, 1, ETileType::TILE_512);
+        display.DrawTile(2, 2, ETileType::TILE_1024);
+        display.DrawTile(2, 3, ETileType::TILE_2048);
+
+        // Example: you can draw tile at intermediate point to make animation
+        double elapsedTime = display.GetTime() - oldTime;
+        oldTime = display.GetTime();
+        if (display.IsKeyPressed(EKey::KEY_LEFT)) {
+            y -= 2 * elapsedTime;
+            if (y < -1.5) {
+                y = -1.5;
+            }
+        } else if (display.IsKeyPressed(EKey::KEY_RIGHT)) {
+            y += 2 * elapsedTime;
+            if (y > 1.5) {
+                y = 1.5;
+            }
+        }
+        display.DrawTile(3, 1.5 + y, ETileType::TILE_64);
+
+        display.Render();
+    }
+    return 0;
+}
---
+++ src/display/display.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <memory>
+
+enum class ETileType {
+    TILE_1,
+    TILE_2,
+    TILE_4,
+    TILE_8,
+    TILE_16,
+    TILE_32,
+    TILE_64,
+    TILE_128,
+    TILE_256,
+    TILE_512,
+    TILE_1024,
+    TILE_2048,
+};
+
+enum class EKey {
+    KEY_UP,
+    KEY_DOWN,
+    KEY_LEFT,
+    KEY_RIGHT,
+};
+
+class TDisplay {
+public:
+    TDisplay();
+    virtual ~TDisplay();
+
+    void DrawTile(float x, float y, ETileType type);
+
+    double GetTime() const;
+    bool IsKeyPressed(EKey key) const;
+    bool Closed() const;
+    void ProcessEvents();
+    void Render();
+
+private:
+    class TImpl;
+    std::unique_ptr<TImpl> Impl;
+};
